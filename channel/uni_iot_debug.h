ec45dc482424a355fc37ae6936bfca84940e4203
junlon2006 <junlon2006@163.com>
2020-04-09 17:43:22 +0800

    Format

--- channel/uni_iot_debug.h
+++ channel/uni_iot_debug.h
@@ -1,5 +1,5 @@
 /**************************************************************************
- * Copyright (C) 2020-2020  Unisound
+ * Copyright (C) 2020-2020  Junlon2006
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
